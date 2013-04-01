807c77fcb11aa133a82f235d3dc713fd48908667
ankel <mynameisbinh@gmail.com>
2013-04-01 16:30:15 -0700

    Read input

--- ariadne-skein/Program.cs
+++ ariadne-skein/Program.cs
@@ -9,6 +9,41 @@ namespace ariadne_skein
     {
         static void Main(string[] args)
         {
+            System.IO.StreamReader input = new System.IO.StreamReader(args[0]);
+            
+            String line;
+            int w = 0, h = 0;
+            int i, j;
+
+            while ((line = input.ReadLine()) != null)
+            {
+                ++h;
+                w = Math.Max(w, line.Length);
+            }
+            input.Close();
+
+            char[,] maze = new char[w, h];
+            for (j = 0; j < h; ++j)
+            {
+                for (i = 0; i < w; ++i)
+                {
+                    maze[i, j] = ' ';
+                }
+            }
+            
+            input = new System.IO.StreamReader(args[0]);
+            i = 0;
+            j = 0;
+            while ((line = input.ReadLine()) != null)
+            {
+                foreach (var c in line)
+                {
+                    maze[w, h++] = c;
+                }
+                ++w;
+                h = 0;
+            }
+            
         }
     }
 }
